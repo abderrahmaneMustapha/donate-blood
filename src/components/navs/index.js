c299c16891dad46c38a33955d2583063611d63ff
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-10-20 12:37:46 +0100

    feat: add drop downs

--- src/components/navs/index.js
+++ src/components/navs/index.js
@@ -8,6 +8,7 @@ import {
     Text,
     Button,
     Anchor,
+    Grid,
 } from "grommet";
 import {
     Phone,
@@ -17,35 +18,55 @@ import {
     Twitter,
 } from "grommet-icons";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faAssistiveListeningSystems,
+    faHandHoldingHeart,
+    faHandshake,
+    faSearch,
+    faPills,
+    faProjectDiagram,
+    faPrayingHands,
+    faSmokingBan,
+    faCommentDots
+} from "@fortawesome/free-solid-svg-icons";
+
+//import {useHistory} from "react-router-dom"
+
 const gravatarLink =
     "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80";
 
 const DropContent = (props) => {
     return (
-        <Box
+        <Grid
             animation="slideDown"
             background={{ color: "background", dark: false }}
-            pad="medium"
-            width="70vw"
-            gap="medium"
-            direction="row-responsive"
-            margin={{ top: "2rem" }}
+            pad={{ left: "medium", bottom: "medium", top: "medium" }}
+            width="75vw"
+            gap="small"
+            style={{ overflow: "hidden" }}
+            columns={["1/4", "1/4", "1/4", "1/4"]}
+            margin={{ top: "2rem", left: "2em", right: "2em" }}
         >
             {props.items.map((element) => (
-                <Text>{element}</Text>
+                <>{element}</>
             ))}
-        </Box>
+        </Grid>
     );
 };
 
 const PhoneBox = () => (
     <Box direction="column">
-        <Box direction="row" gap="small" >
-            <Phone color="#FB6929"  weight="bold" size="30em"/>
-            <Text  weight="bold"  color="#FB6929" size="1.5em">00 0000000</Text>
+        <Box direction="row" gap="small">
+            <Phone color="#FB6929" weight="bold" size="30em" />
+            <Text weight="bold" color="#FB6929" size="1.5em">
+                00 0000000
+            </Text>
         </Box>
-        <Text textAlign="center" size="0.7em"> Écoute, aide, soutien</Text>
-       
+        <Text textAlign="center" size="0.7em">
+            {" "}
+            Écoute, aide, soutien
+        </Text>
     </Box>
 );
 
@@ -58,7 +79,7 @@ const SocialList = () => {
     ];
 
     return (
-        <Box direction="row" gap="small">
+        <Box direction="row-responsive" gap="small">
             {social_list.map((element) => (
                 <Anchor
                     href="#"
@@ -69,7 +90,87 @@ const SocialList = () => {
         </Box>
     );
 };
-const items = ["aazeazeazeaze", "bazeazeazeazeazeze"];
+
+const IconsBox = (props) => {
+    return (
+        <Anchor color="#575757" href={props.link}>
+            <Box direction="column" gap="small" width="small" margin={{bottom:"2em"}} align="center">
+                <FontAwesomeIcon
+                    color={props.color}
+                    icon={props.icon}
+                    size="3x"
+                />
+                <Text textAlign="center" size=".7em">
+                    <strong>{props.text}</strong>
+                </Text>
+            </Box>
+        </Anchor>
+    );
+};
+
+const trouve_items = [
+    <IconsBox
+        text="Le Soutien psychologique et l’écoute"
+        icon={faAssistiveListeningSystems}
+        color="#06A991"
+        link="/articles/27991_avec-vous-chaque-etape-de-la-maladie"
+    />,
+];
+const soutiens_items = [
+    <IconsBox
+        text="Faire un don"
+        icon={faHandHoldingHeart}
+        color="#4C99EC"
+        link="/faire-un-don"
+    />,
+    <IconsBox
+        text="Devenir une entreprise partenaire"
+        icon={faHandshake}
+        color="#4C99EC"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />,
+];
+
+const informe_items = [
+    <IconsBox
+        text="Le cancer, définition et chiffres"
+        icon={faSearch}
+        color="#F9496B"
+        link="/faire-un-don"
+    />,
+    <IconsBox
+        text="Traitement et conseil"
+        icon={faPills}
+        color="#F9496B"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />,
+    <IconsBox
+        text="Tous les cancers"
+        icon={faProjectDiagram}
+        color="#F9496B"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />,
+    <IconsBox
+        text="Prévention et dépistage"
+        icon={faPrayingHands}
+        color="#F9496B"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />,
+
+    <IconsBox
+        text="Tabac et cancer"
+        icon={faSmokingBan}
+        color="#F9496B"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />,
+
+    <IconsBox
+        text="Les témoignages"
+        icon={faCommentDots}
+        color="#F9496B"
+        link="/articles/26025_devenir-partenaire-de-la-ligue"
+    />
+];
 export const TopNav = () => {
     const [opendropTrouve, setOpenDropTrouve] = useState(false);
     const [opendropSout, setOpenDropSout] = useState(false);
@@ -77,7 +178,6 @@ export const TopNav = () => {
     return (
         <Header
             background="white"
-            
             pad={{
                 left: "1.2em",
                 right: "1.2em",
@@ -128,13 +228,13 @@ export const TopNav = () => {
                                 setOpenDropSout(false);
                                 setOpenDropInfo(false);
                             }}
-                            onClose={()=>{
+                            onClose={() => {
                                 setOpenDropTrouve(false);
                             }}
                             margin={{ top: "1em" }}
                             open={opendropTrouve}
                             dropAlign={{ top: "bottom", bottom: "bottom" }}
-                            dropContent={<DropContent items={items} />}
+                            dropContent={<DropContent items={trouve_items} />}
                         />
                         <DropButton
                             plain
@@ -145,13 +245,13 @@ export const TopNav = () => {
                                 setOpenDropSout(true);
                                 setOpenDropInfo(false);
                             }}
-                            onClose={()=>{
+                            onClose={() => {
                                 setOpenDropSout(false);
                             }}
                             margin={{ top: "1em" }}
                             open={opendropSout}
                             dropAlign={{ top: "bottom", bottom: "bottom" }}
-                            dropContent={<DropContent items={items} />}
+                            dropContent={<DropContent items={soutiens_items} />}
                         />
 
                         <DropButton
@@ -163,13 +263,13 @@ export const TopNav = () => {
                                 setOpenDropSout(false);
                                 setOpenDropInfo(true);
                             }}
-                            onClose={()=>{
+                            onClose={() => {
                                 setOpenDropInfo(false);
                             }}
                             margin={{ top: "1em" }}
                             open={opendropInfo}
                             dropAlign={{ top: "bottom", bottom: "bottom" }}
-                            dropContent={<DropContent items={items} />}
+                            dropContent={<DropContent items={informe_items} />}
                         />
                     </Box>
                     <SocialList />
